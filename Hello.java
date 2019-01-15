e87e0b35ea477e7521c983166e864800fbc9e554
delacruzc <46725103+delacruzc@users.noreply.github.com>
2019-01-15 14:15:55 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,26 @@
+/***************************
+* HelloPrinter
+*****************
+* Lab Activity
+* ____________________________
+* Caitlin Dela Cruz
+* January 15 2019
+* 255-002
+****************************/
+
+public class Hello {
+	public static void printHeading() {
+
+	System.out.println("Caitlin Dela Cruz");
+	System.out.println("HelloPrinter");
+	System.out.println("First lab activity");
+	System.out.println("1/15/2019");
+	System.out.println("255-002");
+	System.out.println();
+	}
+
+	public static void main(String [] args) {
+		printHeading();
+		System.out.println("Hello");
+	}
+}
